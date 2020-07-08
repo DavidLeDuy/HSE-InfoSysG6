3e5b6aa10e67c3f606d9c8b1706d38e92881cbe2
daleit03 <daleit03@hs-esslingen.de>
2020-07-08 21:39:20 +0200

    added multiple card support for property

--- src/components/propertyList/propertyList.js
+++ src/components/propertyList/propertyList.js
@@ -1,6 +1,8 @@
 const { ipcRenderer } = require("electron");
 const $ = require("jquery");
 const { getPropertyList } = require("../../js/DBFunctions.js");
+const { exists } = require("fs");
+const { create } = require("domain");
 //import { config } from "./config.js";
 
 function changeWindow(wndw) {
@@ -12,22 +14,17 @@ function toggleBtn() {
   $("#scale").toggleClass("scale-out");
 }
 
-function createCard() {
-  var propertyNr = "";
-  var adress = "tesing ";
-  var owner = "some";
-  var units = "stuff";
-  var tenants = "";
+function createCard(property) {
   $("#cardContainer").append(
     '<div class="col s12 m6 l3">' +
-      '<div class="card blue-grey darken-1">' +
+      '<div class="card blue-grey darken-1 hoverable">' +
       '<div class="card-content white-text">' +
-      '<span class="card-title">Property Number: 55</span>' +
+      `<span class="card-title">Property No.${property.propertyNo} ${property.city}</span>` +
       '<ul class="collection">' +
-      `<li class="collection-item">Adress: <span class="right">${adress}</span></li>` +
-      `<li class="collection-item">Owner: <span class="right">${owner}</span></li>` +
-      `<li class="collection-item">Units: <span class="right">${units}</span></li>` +
-      `<li class="collection-item">Tenants: <span class="right"${tenants}</span></li>` +
+      `<li class="collection-item">Adress: <span class="right">${property.postCode} ${property.street} ${property.houseNo}</span></li>` +
+      `<li class="collection-item">Owner: <span class="right">${property.firstName} ${property.lastName}</span></li>` +
+      `<li class="collection-item">Units: <span class="right">${property.numberOfUnits}</span></li>` +
+      `<li class="collection-item">Tenants: <span class="right">${property.numberOfTenants}</span></li>` +
       "</ul>" +
       "</div>" +
       '<div class="card-action">' +
@@ -37,51 +34,53 @@ function createCard() {
       "</div>"
   );
 }
+
 // script to input the data for properties
 $(document).ready(function () {
+  for (let index = 0; index < testJSON.recordset.length; index++) {
+    createCard(testJSON.recordset[index]);
+  }
   getPropertyList()
     .then((propertylist) => {
-      propertylist.recordset.forEach((element) => {});
+      console.dir(propertylist);
+      for (let index = 0; index < propertylist.recordset.length; index++) {
+        createCard(propertylist.recordset[index]);
+      }
     })
     .catch((err) => {
       console.log(err);
     });
-  for (let index = 0; index < 3; index++) {
-    createCard();
-  }
 });
 
-/*
-{
-  recordsets: [ [ [Object], [Object] ] ],
+var testJSON = {
+  recordsets: [[[Object], [Object]]],
   recordset: [
     {
       propertyNo: 2,
       addressNo: 1000000001,
       houseNo: 32,
-      street: 'Amselweg',
+      street: "Amselweg",
       postCode: 67890,
-      city: 'Gﾃｶppingen',
-      firstName: 'Sabine',
-      lastName: 'Mﾃｼller',
-      phoneNo: '01578888',
+      city: "Göppingen",
+      firstName: "Sabine",
+      lastName: "Müller",
+      phoneNo: "01578888",
       numberOfUnits: 12,
-      numberOfTenants: 3
+      numberOfTenants: 3,
     },
     {
       propertyNo: 111,
       addressNo: 999999999,
       houseNo: 11,
-      street: 'Landauerstr',
+      street: "Landauerstr",
       postCode: 70499,
-      city: 'Stuttgart-Weilimdorf',
-      firstName: 'Ekrem',
-      phoneNo: '01234567',
+      city: "Stuttgart-Weilimdorf",
+      firstName: "Ekrem",
+      phoneNo: "01234567",
       numberOfUnits: 3,
-      numberOfTenants: 3
-    }
+      numberOfTenants: 3,
+    },
   ],
   output: {},
-  rowsAffected: [ 2 ]
-}
-*/
+  rowsAffected: [2],
+};
